2ad0773fde6a7d5a9551b4f412df1541d9baa8e4
root <root@dhcp-8-213.nay.redhat.com>
2017-04-14 14:29:09 +0800

    upload some files

---
+++ delete.sh
@@ -0,0 +1,6 @@
+kubectl delete -f heapster-deployment.yaml 
+kubectl delete -f heapster-service.yaml 
+kubectl delete -f influxdb-deployment.yaml
+kubectl delete -f influxdb-service.yaml
+kubectl delete -f grafana-deployment.yaml
+kubectl delete -f grafana-service.yaml
---
+++ start.sh
@@ -0,0 +1,6 @@
+kubectl create -f heapster-deployment.yaml 
+kubectl create -f heapster-service.yaml 
+kubectl create -f influxdb-deployment.yaml
+kubectl create -f influxdb-service.yaml
+kubectl create -f grafana-deployment.yaml
+kubectl create -f grafana-service.yaml
